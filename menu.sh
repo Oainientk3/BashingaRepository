1e3b2985045e8e0fe23687088826a33f0ba6fef0
TintedAspect <lucaskhan1@gmail.com>
2019-10-09 07:42:47 +0100

    added temporary file listing method to menu - to be made more robust by changing out ls -1

--- menu.sh
+++ menu.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+echo These items are currently selectable:
+
+#incremental while loop to display all files in the working repository on separate lines, separated by numerical indicators
+#will not properly handle files with delimiter characters in their titles due to the parsing of ls -1
+i="1"
+x=$(ls -1 | wc -l)
+while [ $i -lt $[$x+1] ]
+do
+	echo -n $i") "; ls -1 | head -$i | tail -1
+	i=$[$i+1]
+done
+
+
 read -p "Which menu item would you like to select? " choice
-echo
 
-if [ $choice = "1" ]
-then
-	echo log 1
-fi
-
-echo You have logged out 1
+ls -1 | head -$choice | tail -1
+echo has been logged out
