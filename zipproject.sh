7925c9a0d9634b8bea9b4c8a9705ea6c91815f65
skyekirwan <43475417+skyekirwan@users.noreply.github.com>
2019-10-18 19:15:16 +0100

    Functions to create archive and un tar it
    Project files to tar need to be in a project folder

---
+++ zipproject.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+
+createArchive() 
+{
+	read -p "What would you like to call this archive? " dirname
+	mkdir $dirname
+
+	# copy files into this dir
+	# if not in a project folder it tried to copy ./ and error happens..
+
+	#./project is hard coded 
+	cp -r ./project ./$dirname/ 
+
+	#create archive of the file
+	tar -zcvf $dirname.tar $dirname
+
+	#delete original
+	rm -r $dirname
+}
+
+
+openArchive()
+{
+	read -p "What tar file would you like to extract? (dont add .tar) " tarfile
+
+	#if the tar file exists
+	if [ -f $tarfile.tar ]
+	then
+		tar -xvf $tarfile.tar
+	else
+		echo "There is no tar file with that name."
+	fi
+}
+
+#test
+createArchive 
+
+#test
+openArchive
